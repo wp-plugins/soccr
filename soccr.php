aeb7179899816537a374ede290635e429652ff09
rockschtar <rockschtar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-30 20:18:19 +0000

    Changed Version
    git-svn-id: https://plugins.svn.wordpress.org/soccr/trunk@306028 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- soccr.php
+++ soccr.php
@@ -4,7 +4,7 @@
   Plugin URI: http://www.eracer.de/Soccr
   Description: Provides a widget to display the last or next match for a specified team. Currently supporting German Bundesliga 1-3. Powered by openligadb.de
   Author: Stevie
-  Version: 0.81 Beta
+  Version: 0.82 Beta
   Author URI: http://www.eracer.de/soccr
  */
 
